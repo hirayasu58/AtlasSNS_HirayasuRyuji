16e65efd7694c06c945be8bae422244d1ec47328
HIRAYASU7\rhira <r.hirayasu@gmail.com>
2024-09-29 17:54:18 +0900

    アコーディオンメニュー、CSS実装。確認よろしくお願いいたします。

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,18 +1,18 @@
         <div id="head">
+            <!-- "{{-- --}}"でlaravelの関数を使えるようにする。ルーティングでname'index'を用意しておく。 -->
+            <h1><a href="{{ route('index') }}"><img src="{{ asset('/images/atlas.png') }}" class="title"></a></h1>
             <div class="side-user">
-                                    <!-- "{{-- --}}"でlaravelの関数を使えるようにする。ルーティングでname'index'を用意しておく。 -->
-                    <h1><a href="{{ route('index') }}"><img src="{{ asset('/images/atlas.png') }}" class="title"></a></h1>
-                <div id="accordion" class="accordion-container">
-                    <p class="user-name">〇〇さん</p>
-                        <p class="menu-btn active"></p>
-                            <nav>
-                                <ul>
-                                    <li><a class="home" href="{{ route('index') }}">ホーム</a></li>
-                                    <li><a class="profile" href="{{ route('profile') }}">プロフィール</a></li>
-                                    <li><a class="logout" href="{{ route('login') }}">ログアウト</a></li>
-                                </ul>
-                            </nav>
-                    <img src="{{ asset('/images/icon1.png') }}">
-                </div>
+                <p class="user-name">〇〇　さん</p>
+                    <div id="accordion" class="accordion-container">
+                        <p class="menu-btn"></p>
+                        <nav>
+                            <ul>
+                                <li class="li-others"><a class="home" href="{{ route('index') }}">HOME</a></li>
+                                <li class="li-profile"><a class="profile" href="{{ route('profile') }}">プロフィール編集</a></li>
+                                <li class="li-others"><a class="logout" href="{{ route('login') }}">ログアウト</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                <img src="{{ asset('/images/icon1.png') }}" class="icon-img">
             </div>
         </div>
